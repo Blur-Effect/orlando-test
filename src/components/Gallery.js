9b04aba98827cd3c46179df0545cfa13c5d0fd86
OrlandoBurgos <programador3423@gmail.com>
2024-09-14 06:20:30 -0500

    Actualizacion de diseño

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,30 +3,26 @@ import React from 'react';
 import images from '../assets/images';
 import './Gallery.css';
 
+const galleryImages = [
+  { src: images.pexels1, alt: 'Piscina 1' },
+  { src: images.pexels2, alt: 'Piscina 2' },
+  { src: images.pexels3, alt: 'Piscina 3' },
+  { src: images.pexels4, alt: 'Piscina 4' },
+  { src: images.pexels5, alt: 'Piscina 5' },
+  { src: images.pexels6, alt: 'Piscina 6' },
+];
+
 const Gallery = () => {
   return (
     <section className="gallery-section py-5 bg-light">
       <div className="container">
         <h2 className="text-center mb-4">Galería de Proyectos</h2>
-        <div className="row">
-          <div className="col-md-4 mb-4 img-container">
-            <img src={images.pexels1} alt="Piscina 1" className="gallery-image" />
-          </div>
-          <div className="col-md-4 mb-4 img-container">
-            <img src={images.pexels2} alt="Piscina 2" className="gallery-image" />
-          </div>
-          <div className="col-md-4 mb-4 img-container">
-            <img src={images.pexels3} alt="Piscina 3" className="gallery-image" />
-          </div>
-          <div className="col-md-4 mb-4 img-container">
-            <img src={images.pexels4} alt="Piscina 4" className="gallery-image" />
-          </div>
-          <div className="col-md-4 mb-4 img-container">
-            <img src={images.pexels5} alt="Piscina 5" className="gallery-image" />
-          </div>
-          <div className="col-md-4 mb-4 img-container">
-            <img src={images.pexels6} alt="Piscina 6" className="gallery-image" />
-          </div>
+        <div className="gallery-container">
+          {galleryImages.map((image, index) => (
+            <div key={index} className="img-container">
+              <img src={image.src} alt={image.alt} className="gallery-image" />
+            </div>
+          ))}
         </div>
       </div>
     </section>
